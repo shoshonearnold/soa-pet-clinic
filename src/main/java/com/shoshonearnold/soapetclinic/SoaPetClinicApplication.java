7c3a25b9d32a4a9c6738d227e65178b3423b56a3
Shoshone Arnold <eastcoastshony@gmail.com>
2020-10-14 22:48:53 -0400

    updated .gitignore & poms

---
+++ src/main/java/com/shoshonearnold/soapetclinic/SoaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.shoshonearnold.soapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaPetClinicApplication.class, args);
+	}
+
+}
